c4469ab4cbba7fae412f3de6d0997270bc6ad39b
Mx6Y@b7RT <danny_hugh@rocketmail.com>
2019-09-27 17:14:29 -0600

    Commit de todo el proyecto

---
+++ application/views/footer.php
@@ -0,0 +1,12 @@
+</div>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script>window.jQuery || document.write('<script src="/docs/4.3/assets/js/vendor/jquery-slim.min.js"><\/script>')</script><script src="/docs/4.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-xrRywqdh3PHs8keKZN+8zzc5TX0GRTLCcmivcbNJWm2rs5C8PRhcEn3czEjhAO9o" crossorigin="anonymous"></script><script src="https://cdn.jsdelivr.net/npm/docsearch.js@2/dist/cdn/docsearch.min.js"></script><script src="/docs/4.3/assets/js/docs.min.js"></script>
+<script>
+	    $(document).ready(function(){
+			$('#sidebarCollapse').on('click',function(){
+				$('#sidebar').toggleClass('active');
+			});
+		});
+	</script>
+</body>
+</html>
---
+++ application/views/welcome_message.php
@@ -0,0 +1,4 @@
+<div class="container-fluid">
+<h2>Inicio</h2>
+
+</div>
