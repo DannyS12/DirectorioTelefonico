dc597c503a415860e5b5f107a99cdeac0124cf6c
Mx6Y@b7RT <danny_hugh@rocketmail.com>
2019-09-28 11:34:14 -0600

    Base de Datos de Aplicacion

--- dirtelefonico.sql
+++ dirtelefonico.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 28-09-2019 a las 19:31:12
+-- Tiempo de generación: 28-09-2019 a las 19:31:13
 -- Versión del servidor: 10.4.6-MariaDB
 -- Versión de PHP: 7.1.31
 
